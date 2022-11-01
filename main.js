c6fb6df8d3c8bc988d51215778c40736ccf6619f
useful-site <fake_ender@proton.me>
2022-11-01 19:16:43 +0100

    Ajout des fichier pricipaux.

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    title: "Zelestia App",
+    autoHideMenuBar: true,
+    icon: path.join(__dirname, './src/img/logo_color.png'),
+    width: 800,
+    height: 600,
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
